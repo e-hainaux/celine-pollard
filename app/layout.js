331e07d16f2aeacf3d4cc0e995e44759186d0b99
Emilien Hainaux <emilienhainaux@yahoo.fr>
2025-09-04 16:04:21 +0200

    fix border-box sizing

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className="bodyLight">
+      <body className="body">
         <Navbar />
         <main className="mainContainer">{children}</main>
         <Footer />
